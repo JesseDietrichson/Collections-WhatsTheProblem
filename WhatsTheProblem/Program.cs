7f8991138cfcebc34ca65b4c98c051f56ac8565b
Jesse Dietrichson <jesse.dietrichson@xamarin.com>
2019-02-07 03:08:53 -0500

    Add project files.

---
+++ WhatsTheProblem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatsTheProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArray = new int[5];
+
+            testArray[0] = 100;
+            testArray[1] = 200;
+            testArray[2] = 300;
+            testArray[3] = 400;
+            testArray[4] = 500;
+        }
+    }
+}
